656b8b76044f3056dda5140570c2d23a4909f1b8
riccu67 <riccu@ya.ru>
2021-08-18 17:43:55 +0300

    ⚡️ Added user profile page
    - Added file with mixins

--- Maoteam/ClientApp/src/app/user-profile/user-profile.component.ts
+++ Maoteam/ClientApp/src/app/user-profile/user-profile.component.ts
@@ -1,11 +1,31 @@
 ﻿import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: 'user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent implements OnInit {
-  public constructor() {}
+  public managerUserInfoForm: FormGroup;
+  public userPasswordForm: FormGroup;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+  ) {
+    this.managerUserInfoForm = this._formBuilder.group({
+      name: ["", Validators.required],
+      surname: ["", Validators.required],
+      fullName: ["", Validators.required],
+      email: ["", Validators.required],
+      phone: ["", Validators.required],
+    });
+
+    this.userPasswordForm = this._formBuilder.group({
+      oldPassword: ["", Validators.required],
+      newPassword: ["", Validators.required],
+    });
+  }
+
 
   public ngOnInit() {}
 }
